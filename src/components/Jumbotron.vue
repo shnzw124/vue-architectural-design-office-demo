96757605d169c7394a013a73a934d0a10a1e738c
shnzw124 <shnzw124.com@gmail.com>
2021-11-05 21:59:39 +0900

    ジャンボトロン修正

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -1,8 +1,9 @@
 <template>
-  <figure class="jumbotron">
-    <img :src="src" :alt="alt" class="jumbotron__img">
-    <figcaption class="jumbotron__title">{{ text }}</figcaption><!-- /.jumbotron__title -->
-  </figure><!-- /.jumbotron -->
+  <div class="jumbotron" :style="{ backgroundImage: 'url(' + imgPath + ')'}">
+    <div class="jumbotron__inner">
+      <p class="jumbotron__title">{{ text }}</p><!-- /.jumbotron__title -->
+    </div><!-- /.jumbotron__inner -->
+  </div><!-- /.jumbotron -->
 </template>
 
 <script>
@@ -11,11 +12,10 @@ export default {
   props: {
     text: String,
     fileName: String,
-    alt: String
   },
   data (){
     return {
-      src: require(`../assets/${this.fileName}`)
+      imgPath: require(`../assets/${this.fileName}`)
     }
   }
 }
@@ -24,23 +24,18 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   .jumbotron {
-    width: 33.33333%;
-    height: auto;
-    position: relative;
-  }
-  
-  .jumbotron__img {
-    position: absolute;
-    top: 0;
-    left: 0
+    width: 100%;
+    height: 20vw;
+    max-height: 320px;
+    background-position: center center;
+    background-size: cover;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-content: center;
   }
 
   .jumbotron__title {
-    position: absolute;
-    top: 50%;
-    transform: translateY(-100%);
-    left: 240px;
-    transform: translateX(-50%);
     font-size: 40px;
     font-weight: bold;
     font-style: italic;
